d43a4a793c88d8b710a9df684e6be713a364c97c
GuilhermeRodrigues22 <guicordeiro556@gmail.com>
2023-04-18 15:03:31 -0300

    Calculadora IMC - Finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IMCComponent } from './imc/imc.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IMCComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
